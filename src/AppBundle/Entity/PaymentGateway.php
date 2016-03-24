0a00fa7b056dbee60b6f819ab656a428c34e1aed
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-24 15:36:04 +0200

    entity e repository yolu gösterildi

--- src/AppBundle/Entity/PaymentGateway.php
+++ src/AppBundle/Entity/PaymentGateway.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PaymentGatewayRepository")
  * @ORM\Table("payment_gateways")
  * Class PaymentGateway
  * @package AppBundle\Entity
