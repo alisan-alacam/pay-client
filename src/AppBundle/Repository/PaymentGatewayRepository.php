882685c36a3fd7179cf28d326ee120c1a3c6a169
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-24 15:52:36 +0200

    tümünü değil de sadece gerekli alanlar çekilecek şekilde repository düzenlendi

--- src/AppBundle/Repository/PaymentGatewayRepository.php
+++ src/AppBundle/Repository/PaymentGatewayRepository.php
@@ -12,20 +12,22 @@ class PaymentGatewayRepository extends EntityRepository
 {
     /**
      * Aktif payment gateway i slug => value şeklinde döndürür
-     * 
+     *
      * @return array
      */
     public function getActivePaymentGatewayChoices()
     {
         $choices = array();
-        $gateways = $this->getEntityManager()
-            ->getRepository('AppBundle:PaymentGateway')
-            ->findBy(array(
-                'isActive' => 1
-            ));
+        $gateways = $this->createQueryBuilder('pg')
+            ->select('pg.slug, pg.name')
+            ->where('pg.isActive = 1')
+            ->getQuery()
+            ->getArrayResult();
 
-        foreach ($gateways as $gateway) {
-            $choices[$gateway->getSlug()] = $gateway->getName();
+        if (count($gateways) > 0) {
+            foreach ($gateways as $gateway) {
+                $choices[$gateway['slug']] = $gateway['name'];
+            }
         }
 
         return $choices;
