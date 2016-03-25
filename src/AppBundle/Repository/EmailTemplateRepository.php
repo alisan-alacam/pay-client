8318506beea08982a8c92e34d80e740818616197
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:48:39 +0200

    takma isime göre email şablonundaki konu ve içeriği döndürecek metod eklendi

--- src/AppBundle/Repository/EmailTemplateRepository.php
+++ src/AppBundle/Repository/EmailTemplateRepository.php
@@ -11,5 +11,26 @@ use Doctrine\ORM\Query;
  */
 class EmailTemplateRepository extends EntityRepository
 {
-
+    /**
+     * Takma isime göre email şablonundaki konu ve içeriği döndürür
+     *
+     * @param $emailTemplateSlug
+     * @return mixed|null
+     */
+    public function getEmailTemplateBySlug($emailTemplateSlug)
+    {
+        $qb = $this->createQueryBuilder('et')
+            ->select('et.subject, et.content')
+            ->where('et.slug = :slug')
+            ->setParameters(array(
+                'slug' => $emailTemplateSlug
+            ))
+        ;
+        try {
+            $emailTemplate = $qb->getQuery()->getSingleResult(Query::HYDRATE_ARRAY);
+        } catch (\Exception $e) {
+            $emailTemplate = null;
+        }
+        return $emailTemplate;
+    }
 }
