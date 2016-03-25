48607ef3c401f6cbdca9dddb5ad089b9d458e46c
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:47:22 +0200

    email şablon için entity eklendi

---
+++ src/AppBundle/Entity/EmailTemplate.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table("email_templates")
+ * Class EmailTemplate
+ * @package AppBundle\Entity
+ */
+class EmailTemplate
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * @var
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var
+     */
+    private $subject;
+
+    /**
+     * @ORM\Column(type="text")
+     * @var
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var
+     */
+    private $createdAt;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param mixed $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param mixed $content
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param mixed $createdAt
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+    }
+}
