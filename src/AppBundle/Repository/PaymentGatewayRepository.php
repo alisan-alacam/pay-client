1abca689dbe21f54afae37f4e2655a07f05c4472
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:17:51 +0200

    Ödeme metodunun aktiflik durumunu ve ödeme metodu kur bilgilerini döndüren methodlar eklendi

--- src/AppBundle/Repository/PaymentGatewayRepository.php
+++ src/AppBundle/Repository/PaymentGatewayRepository.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
 
 /**
  * Class PaymentGatewayRepository
@@ -31,5 +32,60 @@ class PaymentGatewayRepository extends EntityRepository
         }
 
         return $choices;
+    }
+
+    /**
+     * Ödeme metodunun aktiflik durumunu döndürür
+     *
+     * @param $paymentGatewaySlug
+     * @return bool
+     */
+    public function paymentGatewayIsActive($paymentGatewaySlug)
+    {
+        $qb = $this->createQueryBuilder('pg')
+            ->select('pg.id')
+            ->where('pg.isActive = 1')
+            ->where('pg.slug = :slug')
+            ->setParameters(array(
+                'slug' => $paymentGatewaySlug
+            ))
+        ;
+
+        try {
+            $isActive = $qb->getQuery()->getOneOrNullResult();
+        } catch (\Exception $e) {
+            $isActive = null;
+        }
+
+        if ($isActive !== null) {
+            $isActiveStatus = true;
+        } else {
+            $isActiveStatus = false;
+        }
+        return $isActiveStatus;
+    }
+
+    /**
+     * Ödeme metodunun kur farkı ve kur adını döndürür
+     *
+     * @param $paymentGatewaySlug
+     * @return mixed|null
+     */
+    public function getExchangeRateBySlug($paymentGatewaySlug)
+    {
+        $qb = $this->createQueryBuilder('pg')
+            ->select('pg.defaultExchangeRate, pg.exchangeRateDifference')
+            ->where('pg.isActive = 1')
+            ->where('pg.slug = :slug')
+            ->setParameters(array(
+                'slug' => $paymentGatewaySlug
+            ))
+        ;
+        try {
+            $exchangeRate = $qb->getQuery()->getSingleResult(Query::HYDRATE_ARRAY);
+        } catch (\Exception $e) {
+            $exchangeRate = null;
+        }
+        return $exchangeRate;
     }
 }
