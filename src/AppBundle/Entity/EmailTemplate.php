75973eb48a592ae724a3b39db93bfee94cafe499
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:48:14 +0200

    repository entity e tanımlandı

--- src/AppBundle/Entity/EmailTemplate.php
+++ src/AppBundle/Entity/EmailTemplate.php
@@ -5,7 +5,7 @@ namespace AppBundle\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity()
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\EmailTemplateRepository")
  * @ORM\Table("email_templates")
  * Class EmailTemplate
  * @package AppBundle\Entity
