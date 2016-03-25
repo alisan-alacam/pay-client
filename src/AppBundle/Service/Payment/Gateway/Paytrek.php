d6d047fb0216f2ffeffd96b7e63d4f79f6449d76
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:29:39 +0200

    paytrek ödeme gateway i eklendi.

---
+++ src/AppBundle/Service/Payment/Gateway/Paytrek.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Service\Payment\Gateway;
+
+class Paytrek extends Payment
+{
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    /**
+     * Paypal constructor.
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->em = $entityManager;
+        parent::__construct($entityManager);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function pay()
+    {
+        $this->checkCurrency();
+        $data = array();
+
+        $response = new PaymentResponse($data);
+
+        // Başarılı ise mail gönder
+        if ($response->isSuccessful()) {
+            $this->sendVoucher();
+        }
+        return $response;
+    }
+}
