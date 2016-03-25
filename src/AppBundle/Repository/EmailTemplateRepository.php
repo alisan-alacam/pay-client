b00ad5b4177a08138407a0f21f0f5b51226a2cb9
Alişan ALAÇAM <halimealacam@gmail.com>
2016-03-25 06:47:57 +0200

    email şablonu için repository oluşturuldu

---
+++ src/AppBundle/Repository/EmailTemplateRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query;
+
+/**
+ * Class EmailTemplateRepository
+ * @package AppBundle\Repository
+ */
+class EmailTemplateRepository extends EntityRepository
+{
+
+}
